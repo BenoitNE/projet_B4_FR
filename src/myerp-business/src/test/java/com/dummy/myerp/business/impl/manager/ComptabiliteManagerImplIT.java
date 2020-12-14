d109ffafc23b6e1602adbb3afbce6c3ef3fe76dd
Benoit Nerin <benoitnerin@gmail.com>
2020-12-14 06:28:28 +0100

    checkGetListComptable fonctionne

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplIT.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplIT.java
@@ -1,13 +1,19 @@
 package com.dummy.myerp.business.impl.manager;
 
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.testbusiness.business.BusinessTestCase;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ComptabiliteManagerImplIT extends BusinessTestCase {
 
+    private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
 
     @Test
-    void test (){
-
+    public void checkGetListCompteComptable() {
+        List<CompteComptable> compteComptableList = manager.getListCompteComptable();
+        Assert.assertNotNull(compteComptableList);
     }
 }
