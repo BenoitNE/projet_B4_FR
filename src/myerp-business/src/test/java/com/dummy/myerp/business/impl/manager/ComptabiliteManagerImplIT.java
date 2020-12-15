b64dd75160369d7fbd7f5763667ab791319706d5
Benoit Nerin <benoitnerin@gmail.com>
2020-12-15 22:29:35 +0100

    modification addReference

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplIT.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplIT.java
@@ -1,6 +1,5 @@
 package com.dummy.myerp.business.impl.manager;
 
-import com.dummy.myerp.consumer.dao.contrat.DaoProxy;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
@@ -38,26 +37,27 @@ public class ComptabiliteManagerImplIT extends BusinessTestCase {
         Assert.assertNotNull(ecritureComptableList);
     }
 
-   /* @Test
-    public void addReferenceWhenSequenceEcritureComptableForYearNowDoesntExist () throws NotFoundException {
+    @Test
+    public void addReferenceWhenSequenceEcritureComptableForYearNowDoesntExist () throws NotFoundException, ParseException, FunctionalException {
         EcritureComptable vEcritureComptable;
         vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
         vEcritureComptable.setDate(new Date());
         vEcritureComptable.setLibelle("Libelle");
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
-                null, new BigDecimal(123),
+                null, new BigDecimal(1234),
                 null));
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
                 null, null,
                 new BigDecimal(1234)));
 
-        manager.addReference(vEcritureComptable);
-        Assert.assertEquals("AC-2020/00001", vEcritureComptable.getReference());
-    }*/
+        manager.insertEcritureComptable(vEcritureComptable);
+        //manager.addReference(vEcritureComptable);
+
+    }
 
     @Test
-    public void insertEcritureComptable () throws ParseException, FunctionalException {
+    public void insertEcritureComptable () throws ParseException, FunctionalException, NotFoundException {
         EcritureComptable vEcritureComptable;
         vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("VE", "Vente"));
@@ -72,11 +72,25 @@ public class ComptabiliteManagerImplIT extends BusinessTestCase {
                 new BigDecimal(1234)));
 
         manager.insertEcritureComptable(vEcritureComptable);
+        vEcritureComptable = manager.getEcritureComptableById(1);
+        Assert.assertEquals("VE-2020/00001", vEcritureComptable.getReference());
     }
 
     @Test
-    public void updateEcritureComptable (){
-        EcritureComptable vEcritureComptable = new EcritureComptable();
+    public void updateEcritureComptable () throws NotFoundException, FunctionalException {
+        EcritureComptable vEcritureComptable = manager.getEcritureComptableById(-1);
+        vEcritureComptable.setLibelle("new libelle");
+        manager.updateEcritureComptable(vEcritureComptable);
+        Assert.assertEquals("new libelle", vEcritureComptable.getLibelle());
+    }
+
+    @Test
+    public void deleteEcritureComptable() throws NotFoundException {
+        manager.deleteEcritureComptable(-4);
+        List<EcritureComptable> ecritureComptableList = manager.getListEcritureComptable();
+        for (EcritureComptable ecritureComptable:ecritureComptableList){
+            Assert.assertNotEquals(java.util.Optional.of(-4), ecritureComptable.getId());
+        }
     }
 
     @Test
@@ -101,6 +115,5 @@ public class ComptabiliteManagerImplIT extends BusinessTestCase {
         EcritureComptable vEcritureComptable = manager.getEcritureComptableById(-2);
         Assert.assertEquals("TMA Appli Xxx",vEcritureComptable.getLibelle());
     }
-
 
 }
