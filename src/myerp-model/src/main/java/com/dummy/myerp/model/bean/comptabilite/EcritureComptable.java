346e7377bd9739bda40568cceee1d21dfc7d25e8
Benoit Nerin <benoitnerin@gmail.com>
2021-01-04 10:44:59 +0100

    test integration continue2

--- src/myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
+++ src/myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
@@ -18,6 +18,7 @@ import org.apache.commons.lang3.StringUtils;
  * Bean représentant une Écriture Comptable
  */
 public class EcritureComptable {
+    // toto
 
     // ==================== Attributs ====================
     /** The Id. */
